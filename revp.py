ee3f2bfd1238837671f91970d80a0259ee64cde4
Kevin Wang <kevinwangqi2@gmail.com>
2022-01-13 23:54:23 -0800

    put datasets in a dataset folder + continued working on revp

--- revp.py
+++ revp.py
@@ -1,5 +1,29 @@
-def revp(dna_string):
-    dna_bases_dict = {"A":"T", "T":"A", "G":"C", "C":"G"}
-    reverse_complement = ""
-    for base in dna_string:
-        reverse_complement += dna_bases_dict[base]
+import os
+
+def revp(dnaString):
+    dnaBasesDict = {"A":"T", "T":"A", "G":"C", "C":"G"}
+    reverseComplement = ""
+    for base in dnaString:
+        reverseComplement += dnaBasesDict[base]
+    reverseComplement = reverseComplement[::-1]
+    
+    for i in dnaString:
+        for j in range(4, 13):
+            pass
+            
+
+    print(reverseComplement)
+
+if __name__ == "__main__":
+    # CHANGE CURRENT WORKING DIRECTORY TO THE DIRECTORY WHERE THIS FILE IS PLACED IN
+    # IN THIS CASE, I WOULD HAVE IT IN MY lab-4 FOLDER
+
+    arg = ""
+
+    programFilePath = os.getcwd()
+    datasetFilePath = programFilePath + r"\datasets\rosalind_revp.txt"
+    with open(datasetFilePath, "r") as f:
+        lines = f.readlines()
+
+        arg = lines[1]
+        print(revp(arg))
